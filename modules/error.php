c6b0bdbf8303c69f7a411a9b372ed04e4f5df255
Andres Freites <andrewfreites@gmail.com>
2019-11-21 23:05:42 -0400

    añadida página de error

---
+++ modules/error.php
@@ -0,0 +1,15 @@
+<?php
+header ("refresh:5;url=../index.html");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pagina no encontrada</title>
+</head>
+<body>
+<h2>Ha ocurrido un error, será redireccionado</h2> 
+</body>
+</html>
