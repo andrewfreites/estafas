9697b271e5643cae758cb6dc3179fbf6bcd8702b
andrewfreites <andrewfreites@gmail.com>
2020-01-24 21:27:20 -0400

    Update conexion.php
    removing comments and fix connect error

--- modules/conexion.php
+++ modules/conexion.php
@@ -1,18 +1,17 @@
 <?php
 //conexion realizada con 'mysqli'
-
 //variables de la base de datos
-$servername = "localhost"; //cambiar por el nombre del host del servidor a utilizar
-$database = "db_denuncias"; //nombre de la base de datos
-$username = "root"; // usuario asignado como administrador de la base de datos
-$password = "password"; //clave del usuario asignado como administrador de la base de datos
+$servername = "localhost";
+$database = "db_denuncias";
+$username = "root";
+$password = "sabrent";
 
 //Crear conexion
 $conn = new mysqli($servername, $username, $password, $database);
 
 //revisar la conexion
 
-if(!$conn) {
-	die("Connection failed:" . mysqli_connect_error()); //muestra un mensaje de si la conexion falló y regresa el error
+if($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_errno ." ." . $conn->connect_error); //muestra un mensaje de si la conexion falló y regresa el error
 }
 ?>
